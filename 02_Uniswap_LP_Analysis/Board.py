e6f4efc5312a140373e23f0079500e7e34ee7875
Bella <83490314+bellatrix-ds@users.noreply.github.com>
2025-06-27 16:02:46 +0300

    Update Board.py

--- 02_Uniswap_LP_Analysis/Board.py
+++ 02_Uniswap_LP_Analysis/Board.py
@@ -18,4 +18,4 @@ import google.generativeai as genai
 df = pd.read_csv('https://raw.githubusercontent.com/bellatrix-ds/onchain-analytics/refs/heads/main/02_Uniswap_LP_Analysis/01_uniswap_daily_events_count.csv', on_bad_lines='skip')
 
 st.set_page_config(layout="wide")
-st.title("Board")
+st.title("Uniswap Onchain LP Activity & Yield Monitor")
