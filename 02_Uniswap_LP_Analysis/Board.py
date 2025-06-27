47b84c08c32232f6ce7a2f6a1ab1464c0cfa5726
Bella <83490314+bellatrix-ds@users.noreply.github.com>
2025-06-27 16:00:12 +0300

    Create Board.py

---
+++ 02_Uniswap_LP_Analysis/Board.py
@@ -0,0 +1,21 @@
+
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from matplotlib.dates import DateFormatter
+import numpy as np
+import altair as alt
+from sklearn.preprocessing import MinMaxScaler
+import requests
+import json
+import google.generativeai as genai
+
+# __________________ Import Data ______________________________________________________________________
+
+df = pd.read_csv('https://raw.githubusercontent.com/bellatrix-ds/onchain-analytics/refs/heads/main/02_Uniswap_LP_Analysis/01_uniswap_daily_events_count.csv', on_bad_lines='skip')
+
+st.set_page_config(layout="wide")
+st.title("Board")
