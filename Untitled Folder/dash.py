3416909e7ac000a05aaf5b69ec780985a988b067
Bella <83490314+bellatrix-ds@users.noreply.github.com>
2025-07-01 17:56:11 +0300

    Create dash.py

---
+++ Untitled Folder/dash.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from matplotlib.dates import DateFormatter
+import numpy as np
+import altair as alt
+from sklearn.preprocessing import MinMaxScaler
+import requests
+import json
+import google.generativeai as genai
+
+# __________________ Import Data ______________________________________________________________________
+
+data = pd.read_csv('https://raw.githubusercontent.com/bellatrix-ds/onchain-analytics/refs/heads/main/Untitled%20Folder/df_main.csv', on_bad_lines='skip')
+
+st.title("🔍 Pool Liquidity")
